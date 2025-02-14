199b13d7dbdee227871ee10a58d90d48d7612e6d
shuitrongom <shuitrongomez05@gmail.com>
2025-02-13 22:41:04 -0600

    Update OrdenRepository.java

--- src/main/java/com/trupper/order/api/repository/OrdenRepository.java
+++ src/main/java/com/trupper/order/api/repository/OrdenRepository.java
@@ -1,7 +1,5 @@
 package com.trupper.order.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,7 @@ import com.trupper.order.api.entity.Orden;
 @Repository
 public interface OrdenRepository extends JpaRepository<Orden, Integer> {
 	
-	List<Orden> findByOrdenId(Integer ordenId);
-	List<Orden> findBySucursalId(Integer sucursalId);
+	Orden findByOrdenId(Integer ordenId);
+	Orden findBySucursalId(Integer sucursalId);
 
 }
