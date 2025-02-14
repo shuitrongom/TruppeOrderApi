ecc18af4959f13155a8d6e928bfd95d296887d5d
shuitrongom <shuitrongomez05@gmail.com>
2025-02-13 22:14:02 -0600

    Update ProductoRepository.java

--- src/main/java/com/trupper/order/api/repository/ProductoRepository.java
+++ src/main/java/com/trupper/order/api/repository/ProductoRepository.java
@@ -1,7 +1,5 @@
 package com.trupper.order.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,8 @@ import com.trupper.order.api.entity.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 	
-	List<Producto> findByProductoId(Integer productoId);
-	List<Producto> findByOrdenId(Integer sucursalId);
+	Producto findByProductoId(Integer productoId);
+	Producto findByOrdenId(Integer sucursalId);
+	Producto findByCodigo(String codigo);
 
 }
