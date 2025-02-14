48244a71a2f9c7d94794b5b1cd115c1b45a236c9
shuitrongom <shuitrongomez05@gmail.com>
2025-02-13 22:13:32 -0600

    Update ProductoService.java

--- src/main/java/com/trupper/order/api/service/ProductoService.java
+++ src/main/java/com/trupper/order/api/service/ProductoService.java
@@ -6,11 +6,12 @@ import com.trupper.order.api.entity.Producto;
 
 public interface ProductoService {
 	public List<Producto> findAll();
-	public List<Producto> findByProductoId(Integer codigo);
-	public List<Producto> findByOrdenId(Integer ordenId);
+	public Producto findByProductoId(Integer codigo);
+	public Producto findByOrdenId(Integer ordenId);
 	public Producto findById(int productoId);
 	public Producto save(Producto producto);
 	public Producto update(Producto producto);
+	public Producto updateForCodigo(Producto producto);
 	public void delete(int productoId);
 
 }
